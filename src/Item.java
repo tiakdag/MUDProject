07ea0df80007b0280a5631a2857e5c01665e3120
takdag <takdag@phoenix.goucher.edu>
2016-09-15 13:14:03 -0400

    Initial commit

---
+++ src/Item.java
@@ -0,0 +1,31 @@
+/** A class that creates an item 
+ * 
+ * @author takdag
+ *
+ */
+public class Item {
+/**
+ * Constructs a Item object that has a name and can be picked up.
+ * @param x the name of the object that will be picked up
+ */
+	public Item(String x){
+		name = x;
+	}
+	/**
+	 * getName() gets the name of the object and returns that name
+	 * @return the name of the object
+	 */
+	public String getName(){
+		return name;
+	}
+	/**
+	 * pickUpItem takes the item and will add it to the inventory.
+	 * 
+	 * @return a message saying the player has picked up a item 
+	 */
+	public String pickUpItem(){
+		return "You have picked up " + name;
+	}
+	
+	private String name;
+}

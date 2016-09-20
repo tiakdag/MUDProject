d02107f26cf41b00f5ad17c6d7f69fb6e05fe8c9
BigBootySaeks <nebski123@yahoo.com>
2016-09-20 11:27:51 -0400

    Upload 11:27

--- Room.java
+++ Room.java
@@ -1,10 +1,13 @@
+import java.util.*;
 
 public class Room {
 /**
  * constructs a room object that has a name and can be entered
- * @param x the name of the room that will be entered
+ * @param x the name of the room that will be entered.
+ * @param items is the list of items in the inventory of the room.
  */
-	public Room(String x){
+	public Room(String x,Item items){
+		Inventory I = new Inventory(items);
 		name = x;
 	}
 	/**
@@ -13,6 +16,17 @@ public class Room {
 	 */
 	public String getRoom() {
 		return name;
+	}
+	
+	/**
+	 * Takes in player query and sends back an Item if it is in the room or a null response if
+	 * the item is not there.
+	 * @param item_query is a string from the UI to the Player Class to room that represents the item the player wishes to pick up.
+	 * @return Item object if the the desired item is in the room and accessible.
+	 */
+	public Item getItem(String item_query){
+		
+		
 	}
 	private String name;
 }

919ae58be78daadf0481019326cbb81ae7e70631
tiakdag <tiakd001@mail.goucher.edu>
2016-09-20 10:09:03 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,6 +1,24 @@
 
+/**
+ * A class that creates a player
+ * @author takdag
+ *
+ */
 public class Player {
-
-	
+	/**
+	 * Constructs a player object that has a name and 
+	 * @param x the name of the player
+	 */
+	public Player(String x) {
+		name = x;
+	}
+	/**
+	 * getName() gets the name of the player and returns that name
+	 * @return the name of the player
+	 */ 
+	public String getName() {
+		return name;
+	}
 	private String name;
 }
+

40cfb669fb40c91c6c6343fb88e1df720ca0b23e
tiakdag <tiakd001@mail.goucher.edu>
2016-09-20 10:09:57 -0400

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,11 +1,19 @@
 
 public class Room {
-
+/**
+ * constructs a room object that has a name and can be entered
+ * @param x the name of the room that will be entered
+ */
 	public Room(String x){
 		name = x;
 	}
+	/**
+	 * getRoom() gets the name of the room
+	 * @return the name of the room
+	 */
 	public String getRoom() {
 		return name;
 	}
 	private String name;
 }
+

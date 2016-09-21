d891ed0d35dd10a1a6601c065d1450f201c692ad
tiakdag <tiakd001@mail.goucher.edu>
2016-09-20 20:38:49 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,3 @@
-
 /**
  * A class that creates a player
  * @author takdag
@@ -11,6 +10,7 @@ public class Player {
 	 */
 	public Player(String x) {
 		name = x;
+		
 	}
 	/**
 	 * getName() gets the name of the player and returns that name
@@ -19,6 +19,35 @@ public class Player {
 	public String getName() {
 		return name;
 	}
+	
+	/* public String location() {
+	 * return the current location of the player
+	 */
+	
+	/*public Boolean pickUpItem(String x){
+		this method allows player to add an item to their inventory
+		User enters pick up item.
+		player calls on the inventory of the room to see if item is in room.
+		If item is in room, then remove the item from inventory of room 
+		to inventory of the player.
+		
+	}*/
+	
+	
+	/*public Boolean moveRooms(String x) {
+	 * this method moves players from one room to the next
+	 * if player enters a correct direction then player moves current location -> new location
+	 * if player enters wrong direction player stays 
+	 *  if (player enters up) {
+	 *  then (changeRoom up)
+	 *  else if (player enters down) 
+	 *  then (changeRoom down)
+	 *  else if (player enters right)
+	 *  then (changeRoom right)
+	 *  else if (player enters left)
+	 *  then (changeRoom left)
+	 *  else (player stays)
+	 */
+	 
 	private String name;
 }
-

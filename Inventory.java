d26ebf3eb0aff0ead766b64ddce51f18949f16d9
tiakdag <tiakd001@mail.goucher.edu>
2016-09-22 11:05:22 -0400

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,17 +1,24 @@
 import java.util.ArrayList;
 public class Inventory {
+	
 	/**
-	 * Creates an inventory object that contains an array of items that a player can pick up
-	 * @param x 
+	 * Creates an inventory object that contains an array of items 
+	 * @param Item[] x an array of items that are entered from other classes
 	 */
 	
 	//  List<String> list = new ArrayList<String>();
-	public Inventory(){
-		ArrayList Item = new ArrayList(10);
-		ArrayList Room = new ArrayList(10);
-		ArrayList PlayerInventory = new ArrayList(10);
-		ArrayList MOB = new ArrayList(10);
-	}
+	public Inventory(Item[] x ){  //or MOB
+ 		ArrayList myInventory = new ArrayList  (40);
 	
+		for (int i=0; i <= x.length; i++){
+			myInventory.add(x[i]);
+		}
+	}
+	public Item getItem(String x){
+		if (this.contains(x)){
+		this.remove(x);
+		return ()
+		}else return null;
+	}
 	
 }
